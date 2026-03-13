7f70a25f6771bf6eb82238d037ffc0678a03dfe4
Mauricio Chaves <mauricio.chaves.jr@gmail.com>
2026-03-13 14:04:03 -0300

    Criação do Endppoint Login com JWT token

---
+++ utils/users-list-bd.js
@@ -0,0 +1,16 @@
+const USERS_LIST_BD = [
+    {
+        name: 'mauricio',
+        email: 'mauricio@email.com',
+        username: 'mauricio',
+        password: 123
+    },
+    {
+        name: 'ana',
+        email: 'ana@email.com',
+        username: 'ana',
+        password: 123
+    }
+];
+
+module.exports = { USERS_LIST_BD };
